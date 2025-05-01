01dec6c0ee49188b8d8051cf618527fb4e8e205c
Syndher <miguelfilipe.dev@gmail.com>
2025-05-01 19:51:00 +0100

    Added PlayerManager4

---
+++ PlayerManager4/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.Emit;
+
+namespace Players
+{
+    public class Player : IComparable<Player>
+    {
+        public string Name { get;  }
+        public int Score { get;  }
+        public Player(string name, int score)
+        {
+            Name = name;
+            Score = score;
+        }
+        public override string ToString()
+        {
+            return $"{Name} - Score: {Score}";
+        }
+        public int CompareTo(Player other)
+        {
+            if (other == null) return 1;
+            return other.Score.CompareTo(this.Score);
+            
+        }
+        
+    }
+    
+}
