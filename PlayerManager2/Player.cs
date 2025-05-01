c73aa8692b0837eb3462dc1851839b9019f1e70d
Syndher <miguelfilipe.dev@gmail.com>
2025-05-01 19:24:04 +0100

    PlayerManager2 with yield return

---
+++ PlayerManager2/Player.cs
@@ -0,0 +1,18 @@
+﻿namespace Players
+{
+    public class Player
+    {
+        public string Name { get;  }
+        public int Score { get;  }
+        public Player(string name, int score)
+        {
+            Name = name;
+            Score = score;
+        }
+        public override string ToString()
+        {
+            return $"{Name} - Score: {Score}";
+        }
+        
+    }
+}
